cfac628d217278c1d01ff820dc02fcab0236c921
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-06-08 14:55:25 -0300

    Header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../assets/ignite-logo.svg"
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
       <strong>Ignite Feed</strong>
     </header>
   )
