127521af8840235a8a829ec30054b046e63b4494
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-06-08 14:48:09 -0300

    aplicando estilos padrões

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { Header } from "./components/Header";
+
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <main>
+      <Header />
+      <h1>Hello World</h1>
+    </main>
   )
 }
