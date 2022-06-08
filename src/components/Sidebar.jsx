2100ec5a3b6ff03f04b122610b2c76cc4405e98b
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-06-08 20:57:10 -0300

    Sidebar finalizada

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -5,11 +6,12 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1654455103120-e33ac58ab56a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/72226013?v=4" />
         <strong>Rafael Manfrim</strong>
         <span>Web developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#"><PencilLine size={20} />  Editar seu perfil</a>
       </footer>
     </aside>
   )
